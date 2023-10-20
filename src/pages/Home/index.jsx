395f2be1d5443314a0fe2be9aa1d5c208f7d38ce
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-20 15:02:40 -0600

    portfolio-codeprojects

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.scss';
+
+function Home() {
+  return (
+    <div className="home">
+      <header>
+        <h1>Bienvenido a mi página de inicio</h1>
+      </header>
+      <main>
+        <p>Esta es una página de inicio simple.</p>
+      </main>
+      <footer>
+        <p>© 2023 - Mi Página de Inicio</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Layout/index.jsx
@@ -0,0 +1,12 @@
+import './index.scss'
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+  return (
+    <div className="App">      
+       <Outlet></Outlet>     
+    </div>
+  )
+}
+
+export default Layout
